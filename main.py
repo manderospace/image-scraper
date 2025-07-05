6f7ced87c08a65143c566ec680f30228144ccd8d
manderospace <manderospace@gmail.com>
2025-07-05 02:58:28 +0200

    fix query

--- main.py
+++ main.py
@@ -21,9 +21,15 @@ def scrape_reddit_images(query, subreddit="kpics", limit=10):
         results = sub.search(query, limit=limit)
         urls = []
         for post in results:
-            url = post.url
+            url = post["data"].get("url", "")
             if url.lower().endswith((".jpg", ".jpeg", ".png", ".gif", ".webp")):
-                urls.append(url)
+                urls.append(fix_imgur_url(url))
+            elif "reddit.com/gallery/" in url and "media_metadata" in post["data"]:
+                media = post["data"]["media_metadata"]
+                if media:
+                    first_image = next(iter(media.values()))
+                    img_url = first_image["s"]["u"].replace("&amp;", "&")
+                    urls.append(img_url)
         return urls
     except Exception as e:
         print("OAuth Reddit error:", e, flush=True)
@@ -37,19 +43,6 @@ def fix_imgur_url(url):
         return f"https://i.imgur.com/{image_id}.jpg"
     return url
 
-def extract_image_urls(posts):
-    urls = []
-    for post in posts:
-        url = post["data"].get("url", "")
-        if url.lower().endswith((".jpg", ".jpeg", ".png", ".gif", ".webp")):
-            urls.append(fix_imgur_url(url))
-        elif "reddit.com/gallery/" in url and "media_metadata" in post["data"]:
-            media = post["data"]["media_metadata"]
-            if media:
-                first_image = next(iter(media.values()))
-                img_url = first_image["s"]["u"].replace("&amp;", "&")
-                urls.append(img_url)
-    return urls
 
 @app.route("/search")
 def search():
